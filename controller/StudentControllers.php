1cd1bdd99c97088279c5d40cd71d9fc17ff7874b
wissalaitihya <wissalaitihya@gmail.com>
2026-04-04 20:03:03 +0100

    admincontstudents

---
+++ controller/StudentControllers.php
@@ -0,0 +1,83 @@
+<?php
+require_once '../config/db.php';
+require_once '../model/student.php';
+
+class StudentController {
+    private $pdo;
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+}
+//this for admin only to view all students
+public function index() {
+    session_start();
+    //checking if admin
+    if(!isset($_SESSION['logged_in']) || $_SESSION['role'] !== 'admin'){
+     $_SESSION['error'] = "Access denied, for admin only";
+     header("Location: ../view/login.php");
+     exit();
+    }
+    //get all students
+    $students = Student :: getAllStudents($this->pdo);
+    require_once "../view/adminDashboard.php";
+}
+public function showStudentInfo($id){
+    session_start();
+    if(!isset($_SESSION['logged_in']) || $_SESSION['role'] !== 'admin'){
+        $_SESSION['error'] = "Access denied, for admin only";
+        header("Location: ../view/login.php");
+        exit();
+    }
+
+    $student = Student::getStudentById($this->pdo, $id);
+    $lessons = Student::getStudentLessons($this->pdo, $id);
+    $stats = Student::getStudentStatus($this->pdo, $id);
+    if(!$student){
+     $_SESSION['error'] ="Student not found";
+     header("Location: ../view/adminDashboard.php");
+     exit();
+    }
+    require_once "../view/studentDashboard.php";
+}
+//update student info by admin
+public function update(){
+session_start();
+    if(!isset($_SESSION["logged_in"]) || $_SESSION["role"] !== "admin"){
+    $_SESSION["error"] = "Access denied, for admin only";
+    header("Location: ../view/login.php");
+    exit();
+    }
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+         $id = $_POST['student_id'] ?? null;
+         $name = trim($_POST['name'] ?? '');
+         $email = trim($_POST['email'] ?? '');
+         $country = trim($_POST['country'] ?? '');
+         $level = trim($_POST['level'] ?? '');
+    }
+    if(empty($name) || empty($email)){
+        $_SESSION["error"] = "Name and email cannot be empty";
+        header("Location: ../view/adminDashboard.php");
+        exit();
+    }
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $_SESSION["error"] = "Invalid email format";
+        header("Location: ../view/adminDashboard.php");
+        exit();
+    }
+    //UPDATE STUDENT INFO
+    if(student::updateStudent($this->pdo, $id, $name, $country, $level)){
+        $_SESSION["success"] = "Student info updated successfully";
+    }else{
+        $_SESSION['error'] = "Failed to update student info";
+    }
+    header("Location: ../view/adminDashboard.php");
+    exit();
+    }
+
+
+
+
+
+
+
+}
+?>
--- model/student.php
+++ model/student.php
@@ -2,9 +2,89 @@
 require_once('../config/db.php');
 require_once('../model/user.php');
  
+class Student {
+    private $id;
+    private $name;
+    private $country;
+    private $level;
+    private $user_id;
 
-
-
-
-
+    public function __construct ($id, $name, $country, $level, $user_id) {
+        $this ->id = $id;
+        $this ->name = $name;
+        $this ->country = $country;
+        $this ->level = $level;
+        $this ->user_id = $user_id;
+    }
+public function getId() {
+    return $this->id;
+}
+public function getName() {
+    return $this->name;
+}      
+public function getCountry() {
+    return $this->country;
+}
+public function getLevel() {
+    return $this->level;
+}
+public function getUserId() {
+    return $this->user_id;
+}
+public function setNAME($name) {
+    $this->name = $name;
+}
+public function setCountry($country) {
+    $this->country = $country;
+}
+public function setLevel($level) {
+    $this->level = $level;
+}
+public function setUserId($user_id) {
+    $this->user_id = $user_id;
+}
+public static function getAllStudents($pdo){
+    $stmt = $pdo->prepare("SELECT * FROM students WHERE role= 'student' ORDER BY name ASC");
+    $stmt->execute();
+    return $stmt->fetchAll();
+}
+  public static function getStudentById($pdo, $id) {
+    $stmt = $pdo->prepare("SELECT * FROM students WHERE id = ? AND role = 'student'");
+    $stmt->execute([$id]);
+    return $stmt->fetch();
+  }
+public static function getStudentByEmail($pdo, $email){
+    $stmt = $pdo->prepare("SELECT * FROM students WHERE role='student' AND email = ?");
+    $stmt->execute([$email]);   
+    return $stmt->fetch();
+}
+public static function getTotalStudents($pdo){
+    $stmt = $pdo->prepare("SELECT COUNT(*) as total FROM students WHERE role= 'student'");
+   return $stmt->fetch()['total'];
+}
+public static function searchStudents($pdo, $search){
+    $stmt = $pdo->prepare("SELECT * FROM students WHERE role= 'student' AND (name LIKE ? OR email LIKE ?) ORDER BY name ASC");
+    $searchTerm = '%$search%';
+    $stmt->execute([$searchTerm, $searchTerm]);
+    return $stmt->fetchAll();
+}
+public static function deleteStudent($pdo, $id){
+$stmt= $pdo->prepare("DELETE FROM students WHERE id = ? AND role = 'student'");
+return $stmt->execute([$id]);
+}
+public static function updateStudent($pdo, $id, $name, $country, $level){
+    $stmt = $pdo->prepare("UPDATE students SET name = ?, country = ?, level = ? WHERE id = ? AND role = 'student'");
+    return $stmt->execute([$name, $country, $level, $id]);
+}
+public static function getStudentLessons($pdo, $user_id){
+    $stmt = $pdo->prepare("SELECT * FROM enrollements WHERE user_id = ? ");
+    $stmt->execute([$user_id]);
+    return $stmt->fetchAll();
+}
+public static function getStudentStatus($pdo, $user_id){
+   $stmt = $pdo ->prepare("SELECT status FROM enrollements WHERE user_id = ? ");
+    $stmt->execute([$user_id]);
+    return $stmt->fetchAll();
+}
+}
 ?>
